d8d18bd8e99fa1fa5e5bf1d1212eea136f7ace61
Oruchi Opara <adadevops28@gmail.com>
2024-07-17 00:15:52 -0400

    Updated files for 06

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c3-local-values.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c3-local-values.tf
@@ -1,11 +1,23 @@
+# # Define Local Values in Terraform
+# locals {
+#   owners = var.business_divsion
+#   environment = var.environment
+#   name = "${var.business_divsion}-${var.environment}"
+#   #name = "${local.owners}-${local.environment}"
+#   common_tags = {
+#     owners = local.owners
+#     environment = local.environment
+#   }
+# } 
+
 # Define Local Values in Terraform
 locals {
-  owners = var.business_divsion
+  owners      = var.business_divsion
   environment = var.environment
-  name = "${var.business_divsion}-${var.environment}"
-  #name = "${local.owners}-${local.environment}"
+  name        = "${var.business_divsion}-${var.environment}"
+  # name = "${local.owners}-${local.environment}"
   common_tags = {
-    owners = local.owners
+    owners      = local.owners
     environment = local.environment
   }
-} 
+}
