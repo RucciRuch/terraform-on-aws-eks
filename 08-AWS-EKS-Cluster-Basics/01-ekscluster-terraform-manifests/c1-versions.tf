594e03fa71e767baa7c42a86f95cf7625e91e1c2
Oruchi Opara <adadevops28@gmail.com>
2024-07-19 21:04:25 -0400

    Updated 08 files

--- 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c1-versions.tf
+++ 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c1-versions.tf
@@ -1,12 +1,11 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = "~>1.9.2"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      #version = ">= 4.65"
-      version = ">= 5.31"
-     }
+      source  = "hashicorp/aws"
+      version = "~>5.58"
+    }
   }
 }
 
