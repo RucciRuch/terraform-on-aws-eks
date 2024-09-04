2cb0e7002aba00d19d39479cf7cf2789fdce4474
Oruchi Opara <adadevops28@gmail.com>
2024-09-03 22:42:38 -0400

    Updated files for 29

--- 29-EKS-Ingress-SSL-SSLRedirect/04-ingress-ssl-terraform-manifests/c11-acm-certificate.tf
+++ 29-EKS-Ingress-SSL-SSLRedirect/04-ingress-ssl-terraform-manifests/c11-acm-certificate.tf
@@ -1,6 +1,6 @@
 # Resource: ACM Certificate
 resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "*.stacksimplify.com"
+  domain_name       = "*.kubeoncloud1.com"
   validation_method = "DNS"
 
   tags = {
@@ -9,12 +9,12 @@ resource "aws_acm_certificate" "acm_cert" {
 
   lifecycle {
     create_before_destroy = true
-   }
+  }
 }
 
 # Outputs
 output "acm_certificate_id" {
-  value = aws_acm_certificate.acm_cert.id 
+  value = aws_acm_certificate.acm_cert.id
 }
 
 output "acm_certificate_arn" {
