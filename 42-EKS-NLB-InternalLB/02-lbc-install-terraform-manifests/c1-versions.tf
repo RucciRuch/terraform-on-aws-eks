7885dc4b9b4b5061e735daa1ba1422ed8421860c
Oruchi Opara <adadevops28@gmail.com>
2024-09-09 01:50:51 -0400

    Updated files for 42

--- 42-EKS-NLB-InternalLB/02-lbc-install-terraform-manifests/c1-versions.tf
+++ 42-EKS-NLB-InternalLB/02-lbc-install-terraform-manifests/c1-versions.tf
@@ -1,34 +1,34 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = "~> 1.9.2"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       #version = ">= 4.65"
-      version = ">= 5.31"
+      version = "~> 5.58"
      }
     helm = {
       source = "hashicorp/helm"
       #version = "2.5.1"
       #version = "~> 2.5"
-      version = "~> 2.9"
+      version = "~> 2.15.0"
     }
     http = {
       source = "hashicorp/http"
       #version = "2.1.0"
       #version = "~> 2.1"
-      version = "~> 3.3"
+      version = "~> 3.4.4"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.20"
+      version = "~> 2.32.0"
     }      
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "terraform-on-aws-eks"
+    bucket = "terraform-on-aws-eks-oao"
     key    = "dev/aws-lbc/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-2" 
 
     # For State Locking
     dynamodb_table = "dev-aws-lbc"    
