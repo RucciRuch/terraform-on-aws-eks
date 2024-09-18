776f372dc0a4254c8c1b511c5d1e124c8a1b902a
Oruchi Opara <adadevops28@gmail.com>
2024-09-18 18:51:55 -0400

    Updated files for 48

--- 48-EKS-EFS-Dynamic-Provisioning/03-efs-dynamic-prov-terraform-manifests/c2-remote-state-datasource.tf
+++ 48-EKS-EFS-Dynamic-Provisioning/03-efs-dynamic-prov-terraform-manifests/c2-remote-state-datasource.tf
@@ -3,8 +3,8 @@
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket = "terraform-on-aws-eks"
+    bucket = "terraform-on-aws-eks-oao"
     key    = "dev/eks-cluster/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-2" 
   }
 }
