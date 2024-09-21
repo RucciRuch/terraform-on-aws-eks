5f527ebac92062db92ef35226b397b60bb47352b
Oruchi Opara <adadevops28@gmail.com>
2024-09-21 11:27:57 -0400

    Updated files for 53

--- 53-EKS-Vertical-Pod-Autoscaler-Install/03-vpa-install-terraform-manifests/c1-versions.tf
+++ 53-EKS-Vertical-Pod-Autoscaler-Install/03-vpa-install-terraform-manifests/c1-versions.tf
@@ -1,21 +1,21 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = "~> 1.9.2"
   required_providers {
     null = {
-      source = "hashicorp/null"
-      version = "~> 3.1"
+      source  = "hashicorp/null"
+      version = "~> 3.2.3"
     }
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "terraform-on-aws-eks"
+    bucket = "terraform-on-aws-eks-oao"
     key    = "dev/eks-vpa-install/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-2"
 
     # For State Locking
-    dynamodb_table = "dev-eks-vpa-install"    
-  }     
+    dynamodb_table = "dev-eks-vpa-install"
+  }
 }
 
 provider "null" {
--- 53-EKS-Vertical-Pod-Autoscaler-Install/03-vpa-install-terraform-manifests/c2-vpa-install.tf
+++ 53-EKS-Vertical-Pod-Autoscaler-Install/03-vpa-install-terraform-manifests/c2-vpa-install.tf
@@ -4,7 +4,8 @@
 # Resource-1: Null Resource: Clone GitHub Repository
 resource "null_resource" "git_clone" {
   provisioner "local-exec" {
-    command = "git clone git@github.com:kubernetes/autoscaler.git"
+    #    command = "git clone git@github.com:kubernetes/autoscaler.git"
+    command = "git clone https://github.com/kubernetes/autoscaler.git"
   }
 }
 
@@ -12,16 +13,16 @@ resource "null_resource" "git_clone" {
 # # Resource-2: Null Resource: Install Vertical Pod Autoscaler
 resource "null_resource" "install_vpa" {
   depends_on = [null_resource.git_clone]
- provisioner "local-exec" { 
+  provisioner "local-exec" {
     command = "${path.module}/autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh"
   }
 }
 
 # Resource-3: Null Resource: Remove autoscaler folder
 resource "null_resource" "remove_git_clone_autoscaler_folder" {
- provisioner "local-exec" { 
+  provisioner "local-exec" {
     command = "rm -rf  ${path.module}/autoscaler"
-    when = destroy
+    when    = destroy
   }
 }
 
@@ -29,9 +30,9 @@ resource "null_resource" "remove_git_clone_autoscaler_folder" {
 # Resource-4: Null Resource: Uninstall Vertical Pod Autoscaler
 resource "null_resource" "uninstall_vpa" {
   depends_on = [null_resource.remove_git_clone_autoscaler_folder]
- provisioner "local-exec" { 
+  provisioner "local-exec" {
     command = "${path.module}/autoscaler/vertical-pod-autoscaler/hack/vpa-down.sh"
-    when = destroy
+    when    = destroy
   }
 }
 
